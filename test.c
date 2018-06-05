fc1d2a3796cf08e2a7189c8b42e456a8ae4f519b
Hussem Ben Belgacem <benbel_h@epita.fr>
2018-06-05 12:00:07 +0200

    [LAPACK] Example of qr factorization with lapack

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <lapacke.h>
+
+int main(){
+	double matrix[9] = {1,2,3,4,5,6,7,8,9};
+	int m = 3;
+	int n = 3;
+	int info = 0;
+	int lda = m;
+	int lwork = n;
+	double work[3];
+	double tau[3];
+	dgeqrfp_(&m, &n, matrix, &lda, tau, work, &lwork, &info);
+
+	for (int i = 0; i < m; i++){
+		for (int j = 0 ; j < n; j++)
+			printf(" %f ", matrix[i + j * m]);
+		printf("\n");
+	}
+}
